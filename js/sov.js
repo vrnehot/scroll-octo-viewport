4359fff258b0bc9e0260d6900c8db3e292756c84
vrnehot <vrnehot@yandex.ru>
2015-04-02 15:27:38 +0300

    Сомнения

--- js/sov.js
+++ js/sov.js
@@ -1,15 +1,36 @@
 (function(window,$,und){
 	var sovs = {};
 	var hb = $('html,body');
-	function sov(opt){ return new fn.init(this,opt); }
+	var isTouchDevice = /(iPhone|iPod|iPad|Android|playbook|silk|BlackBerry|BB10|Windows Phone|Tizen|Bada|webOS|IEMobile|Opera Mini)/.test(navigator.userAgent);
+	var isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0) || (navigator.maxTouchPoints));
 
+	/*helpers*/
+	function isReallyTouch(e){ return e.pointerType === und || e.pointerType != 'mouse'; }
+	function getMousePos(e){
+		var pos = {
+			x:(e.pageX !== und ? e.pageX : e.touches[0].pageX)
+			, y:(e.pageY !== und ? e.pageY : e.touches[0].pageY)
+		};
+		if(isTouch && isReallyTouch(e)){
+			pos.x = e.touches[0].pageX;
+			pos.y = e.touches[0].pageY;
+		}
+		return pos;
+	}
+	/*helpers*/
+
+
+	function sov(opt){ return new fn.init(this,opt); }
 	var fn = {
-		init:function($root,opt){
+		init:function(root,opt){
 			var z = this;
-			z.$root = $root;
+			z.root = root;
+			z.$root = $(root);
 			if(opt){$.extend(z,opt);}
 			var t = new Date().getTime(); while(!sovs['sov_'+t]){ t--; }
 			z._id = 'sov_' + t; sovs[z._id] = z;
+
+			console.info('Сомнения ',z.root);
 		}
 		, getInstance:function(id){
 			if(id){ return sovs[id]; }
@@ -19,14 +40,6 @@
 			var z = this;
 			if(z.scrollInner){ z.$root.scrollTop(top); }
 			else{ hb.scrollTop(z.start+top); }
-		}
-		, getMousePos:function(e){
-			var pos = {
-				x:
-				, y:
-			};
-
-			return pos;
 		}
 		, resizeHandler:function(){
 			// change page dim
@@ -38,7 +51,5 @@
 		, hashHandler:function(e){}
 	};
 	$.extend(fn.init.prototype,fn);
-
-
 	$.fn.sov = sov;
 })(window,jQuery);
